669341e72da44e79458bc59341b76056733f521c
Arpit Mehrotra <amehrotr@opentext.com>
2019-06-30 04:14:09 +0530

    Template Literals

---
+++ TemplateLiterals/main.js
@@ -0,0 +1,12 @@
+console.log('string text line 1\n' + 
+'string text line 2');
+
+console.log(`string text line 1
+string text line 2`);
+
+var a = 5;
+var b = 10;
+
+console.log(`Fifteen is ${a+b} and 
+not ${2*a+b}`);
+
